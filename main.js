74292a6b29ea29a60eef9e1275f818ede66cb07d
Mira <kruzinka@yandex.ru>
2023-05-06 18:43:39 +0300

    dropdown rooms js (not done)

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
-/* выпадающий список этажей */
-
+/* выпадающие списки этажей */
 let floors = [];
+let rooms = [];
+
 for (let i = 3; i <= 27; i++) {
     floors.push(i);
 }
 
-function dropdown_list(arr) {
-    const dropSelect = document.querySelector('.form-select-floor');
-    const selectDiv = document.querySelector('.select-dropdowns__floor');
+function dropdownLists(arr) {
+    const dropSelectFloor = document.querySelector('.form-select-floor');
+    const selectDivFloor = document.querySelector('.select-dropdowns__floor');
+    const dropSelectRoom = document.querySelector('.form-select-room');
+    const selectDivRoom = document.querySelector('.select-dropdowns__meeting-room');
 
     for (let floor of arr) {
         let option = document.createElement('option');
         option.value = floor - 2;
         option.innerHTML = floor;
-        dropSelect.appendChild(option);
+        dropSelectFloor.appendChild(option);
     }
-    selectDiv.append(dropSelect);
+    
+
+    dropSelectFloor.addEventListener('change', function() {
+        const selectedFloor = parseInt(dropSelectFloor.value) + 2;
+        // alert(selectedFloor);
+
+        let rooms = getNumbersOfRooms(selectedFloor);
+
+        for (let room of rooms) {
+            let option = document.createElement('option');
+            option.value = room;
+            option.innerHTML = room;
+            dropSelectRoom.appendChild(option);
+        }
+    });
+    
+    selectDivFloor.append(dropSelectFloor);
+    selectDivRoom.append(dropSelectRoom);
 }
-dropdown_list(floors);
+
+function getNumbersOfRooms(floor) {
+    let diff = floor - 3;
+    let firstOnFloor = diff * 10;
+
+    for (let i = firstOnFloor + 1; i <= firstOnFloor + 10; i++) {
+        rooms.push(i);
+    }
+    return rooms;
+}
+
+dropdownLists(floors);
+
+// console.log(getNumbersOfRooms(5));
+
